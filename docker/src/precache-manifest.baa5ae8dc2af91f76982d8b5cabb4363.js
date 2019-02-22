15acc26b55a1b0b1568b0054b24491196a61054b
hroft <sagdinov@gmail.com>
2019-02-22 13:46:01 +0300

    clear code + css

---
+++ docker/src/precache-manifest.baa5ae8dc2af91f76982d8b5cabb4363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "580f704203294d45f7dc",
+    "url": "/static/js/main.580f7042.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "580f704203294d45f7dc",
+    "url": "/static/css/main.c4c3be76.chunk.css"
+  },
+  {
+    "revision": "0432d887f7761d16626aee8accc1a9c4",
+    "url": "/index.html"
+  }
+];
